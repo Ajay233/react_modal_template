51ca9e1280d0811f63d213765c7c0039842a6ccb
Ajay <ajaymungur@hotmail.com>
2020-06-15 17:02:55 +0100

    Reducer and redux dev tool wired up and also applied redux-thunk middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux'
+import { createStore, compose, applyMiddleware } from 'redux';
+import { rootReducer } from './reducers'
+import thunk from 'redux-thunk'
 import App from './App';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
